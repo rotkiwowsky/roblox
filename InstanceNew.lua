a625e9299312973c97ddd6b906b42c0a77471533
rotkiwowsky <rotkiwowsky@gmail.com>
2021-04-14 18:47:21 +0200

    instance new script

---
+++ InstanceNew.lua
@@ -0,0 +1,17 @@
+local button = game.Workspace.Button
+local buttonClickDetector = button.ClickDetector
+
+buttonClickDetector.MouseClick:Connect(function()
+
+	local part = Instance.new("Part")
+	
+	part.Position = Vector3.new(11, 0.5, 7.4)
+	
+	part.TopSurface = 0
+	part.BottomSurface = 0
+	
+	part.Size = Vector3.new()
+
+part.Parent = game.Workspace
+	
+end)
